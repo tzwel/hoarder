f1517e5d9736b2af148fd334ae43d3739a0ddc84
tzwel <tzwel@int.pl>
2023-01-21 21:56:34 +0100

    improve cli

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
 const { downloadSite } = require('./hoarder')
 const prompt = require('prompt-sync')();
+const fs = require('fs');
+const open = require('open');
+
+if (!fs.existsSync('./sites/images')){
+    fs.mkdirSync('./sites/images', { recursive: true });
+    fs.mkdirSync('./sites/styles', { recursive: true });
+}
 
 let site = {
     url: '',
     name: ''
 }
 
+console.info(`
+Welcome to the Hoarder CLI!
+What would you like to do?
+`)
 
-site.url = prompt('Wklej linka (w prostej postaci prosze, z https) \n > ');
+function askForAction() {
+    const action = prompt(`Actions: [O]pen a website or [S]ave a website \n >`);
+    switch (action) {
+        case 'o':
+            openAWebsite()
+            break;
 
-site.name = prompt('Podaj nazwe stronki ktora chcesz pobrac (to dla ciebie) \n > ')
+        case 's':
+            saveAWebsite()
+            break;
 
+    
+        default:
+            askForAction()
+            break;
+    }
+}
 
-downloadSite(site.url, site.name)
+askForAction()
 
+function saveAWebsite() {
+    site.url = prompt('Paste a link to a website here: \n > ');
+    site.name = prompt('Name of the file it will be stored in \n > ')
+    downloadSite(site.url, site.name)
+}
+
+async function openAWebsite() {
+    const availableWebsites = []
+    console.info('\n Available websites:')
+    fs.readdirSync('./sites').forEach(file => {
+        if (file.endsWith('.html')) console.log(`> [${availableWebsites.length}] ${file}`), availableWebsites.push(file)
+    });
+    const websiteToOpen = availableWebsites[Number(prompt('>'))]
+    await open(__dirname + '/sites/' + websiteToOpen)
+}
